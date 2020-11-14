0aadf61f477c458b333702cc6203b58570a862ba
sidinsomniac <chatterjeesid1993@gmail.com>
2020-11-14 11:09:39 +0530

    Initialize campground

---
+++ app.js
@@ -0,0 +1,37 @@
+const express = require('express');
+const app = express();
+const mongoose = require('mongoose');
+const Campground = require('./models/campground');
+
+const path = require('path');
+const port = process.env.PORT || 3000;
+
+mongoose.connect('mongodb://localhost:27017/yelp-camp', { useNewUrlParser: true, useCreateIndex: true, useUnifiedTopology: true });
+
+const db = mongoose.connection;
+db.on('error', console.error.bind(console, "Connection error:"));
+db.once('open', () => {
+    console.log("Datebase connected");
+});
+
+app.set('view engine', 'ejs');
+app.set('views', path.join(__dirname, 'views'));
+
+app.route('/')
+    .get((req, res) => {
+        res.render("home");
+    });
+
+app.route('/campgroundCreate')
+    .get(async (req, res) => {
+        const camp = new Campground({
+            title: "New Camp",
+            description: "Cheap Campground"
+        });
+        await camp.save();
+        res.send(camp);
+    });
+
+app.listen(port, () => {
+    console.log('Server started at ', port);
+});
